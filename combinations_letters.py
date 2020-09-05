f9162fe9211b6bae8354f98e09a20bb2f0b6d2f4
Derek Lee <derekclee232@gmail.com>
2020-09-04 22:26:10 -0700

    cleaned up combinations_letters.py

--- combinations_letters.py
+++ combinations_letters.py
@@ -1,6 +1,10 @@
 from permutation import perm
 
 def comb_letters(word: str):
+    """
+    Produces the possible combinations of the letters of a (str) word
+    For example: PEPPER -> 60
+    """
     letters = {l for l in word}
     divisor = 1
     for i in letters:
